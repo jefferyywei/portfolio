0766716c3922f4e6b7bbf5898603e3cc431dfc23
Jeffery Wei <jefferywei.5765ho7@gmail.com>
2024-01-16 20:39:49 -0500

    added snapchef and resume

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import Logo from './assets/images/logo.png';
 import { TypeAnimation } from 'react-type-animation';
 import corner from './assets/images/corner.png';
+import snapchef from './assets/images/snapchef.png'
 import upn from './assets/images//upn.png'
 import gabbys from './assets/images/gabbys.png'
 import mind from './assets/images/mind.png'
@@ -10,6 +11,7 @@ import church from './assets/images/church.png'
 import insta from './assets/images/instagram.png'
 import github from './assets/images/github.png'
 import linkedin from './assets/images/linkedin.png'
+import resume from './assets/resume.pdf'
 import { Grid } from '@mui/material';
 
 function App() {
@@ -46,7 +48,7 @@ function App() {
               <br />
               <div className='button-container'>
                 <button onClick={handleScroll}> Contact Me</button>
-                <button> Resume</button>
+                <a href={resume} without rel="noopener noreferrer" target="_blank"><button> Resume</button></a>
               </div>
             </div>
           </div>
@@ -64,34 +66,52 @@ function App() {
         <Grid className='cards' container rowSpacing={{ xs: 4, sm: 6, md: 8 , lg:14}} columnSpacing={{ xs: 2, sm: 3, md: 3 , lg:6}}>
           <Grid item lg={4} md={4} sm={6} xs={6}>
             <div className='card'>
+              <a href='https://jwei.pythonanywhere.com/' target='blank'><img src = {snapchef}/></a>
+              <div className='card-flex'>
+                <a className = 'link' href='https://jwei.pythonanywhere.com/' target='blank'>SnapChef</a>
+                <a target = 'blank' href = "https://github.com/BotDestroyer/SnapChef"><img className='git-icon' src={github}/></a>
+              </div>
+            </div>
+          </Grid>
+          <Grid item lg={4} md={4} sm={6} xs={6}>
+            <div className='card'>
               <a href='https://www.upcountypreventionnetwork.org' target='blank'><img src = {upn}/></a>
-              <a className = 'link' href='https://www.upcountypreventionnetwork.org' target='blank'>upcountypreventionnetwork.org</a>
+              <div className='card-flex'>
+                <a className = 'link' href='https://www.upcountypreventionnetwork.org' target='blank'>Upcounty Prevention Network</a>
+                <a target = 'blank' href = "https://github.com/gurujaya/UPN-VersionGuru"><img className='git-icon' src={github}/></a>
+              </div>
             </div>
           </Grid>
           <Grid item lg={4}md={4} sm={6} xs={6}>
             <div className='card'>
               <a href='https://www.thesweetlifetruck.com' target='blank'><img src = {gabbys}/></a>
-              <a className = 'link' href='https://www.thesweetlifetruck.com' target='blank'>thesweetlifetruck.com</a>
+              <div className='card-flex'>
+                <a className = 'link' href='https://www.thesweetlifetruck.com' target='blank'>Gabby's Sweet Treats</a>
+                <a target = 'blank' href = "https://github.com/BotDestroyer/foodtruck"><img className='git-icon' src={github}/></a>
+              </div>
             </div>
           </Grid>
           <Grid item lg={4} md={4}sm={6} xs={6}>
             <div className='card'>
               <a href='https://www.mindlab.cs.umd.edu' target='blank'><img src = {mind}/></a>            
-              <a className = 'link' href='https://www.mindlab.cs.umd.edu' target='blank'>mindlab.cs.umd.edu</a>
+              <a className = 'link' href='https://www.mindlab.cs.umd.edu' target='blank'>MIND Lab</a>
             </div>
           </Grid>
           
-          <Grid item lg={6}md={4} sm={6} xs={6}>
+          <Grid item lg={4}md={4} sm={6} xs={6}>
             <div className='card'>
               <a href='https://www.communityfarmshare.org' target='blank'><img src = {cfs}/></a>
-              <a className = 'link' href='https://www.communityfarmshare.org' target='blank'>communityfarmshare.org</a>
+              <a className = 'link' href='https://www.communityfarmshare.org' target='blank'>Community Farm Share</a>
             </div>
           </Grid>
 
-          <Grid item lg={6} md={4}sm={6} xs={6}>
+          <Grid item lg={4} md={4}sm={6} xs={6}>
             <div className='card'>
               <a href='https://www.churchingaithersburg.org' target='blank'><img src = {church}/></a>
-              <a className = 'link' href='https://www.churchingaithersburg.org' target='blank'>churchingaithersburg.org</a>
+              <div className='card-flex'>
+                <a className = 'link' href='https://www.churchingaithersburg.org' target='blank'>Church In Gaithersburg</a>
+                <a target = 'blank' href = "https://github.com/BotDestroyer"><img className='git-icon' src={github}/></a>
+              </div>
             </div>
           </Grid>
         </Grid>
